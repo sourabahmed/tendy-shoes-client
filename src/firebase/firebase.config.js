d5d185ba26df920b01a534bfa2a8915be70ed751
unknown <you@example.com>
2024-06-03 13:40:30 +0100

    firebase-installed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqBWscNXDSbZHZ7Orfft3_W-s8OGxAbmw",
+  authDomain: "tendy-shoes.firebaseapp.com",
+  projectId: "tendy-shoes",
+  storageBucket: "tendy-shoes.appspot.com",
+  messagingSenderId: "133502862338",
+  appId: "1:133502862338:web:3a3f0053932da65b85131b",
+  measurementId: "G-YV85LS688H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
