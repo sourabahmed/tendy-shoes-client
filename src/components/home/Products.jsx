0fbd5d1d56e07e1d301e4e2b51b3c80ac550b295
unknown <you@example.com>
2024-06-03 19:21:15 +0100

    search-product-added

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -3,6 +3,8 @@ import Product from "../home/Product";
 
 function Products() {
   const [products, setProducts] = useState();
+  const [searchValue, setSearchValue] = useState();
+  console.log(searchValue)
   
   useEffect(() => {
     fetch("http://localhost:3000/products/")
@@ -10,13 +12,19 @@ function Products() {
       .then((data) => setProducts(data?.slice(0, 6)));
   }, []);
 
+
   console.log(products)
   return (
     <>
       <div>
         <h1 className="text-4xl text-center bold p-4">Products</h1>
+        <div className="text-center">
+        <input onChange={e => setSearchValue(e.target.value)} type="text" placeholder="Search shoes here" className="input input-bordered input-info w-full max-w-xs m-5 justify-center" />
+        </div>
         <div className="flex flex-wrap justify-center gap-4">
-      {products?.map((product) => <Product product={product} key={product._id}/>)}
+      {products?.filter((product) => {
+        return searchValue?.toLowerCase() === "" ? product : product?.name?.toLowerCase()?.includes(searchValue)
+      }) .map((product) =>  <Product product={product} key={product._id}/>)}
       </div>
       </div>
     </>
