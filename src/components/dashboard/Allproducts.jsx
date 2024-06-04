771e059cd85c893f6250ea08c48494825f14e9e6
unknown <you@example.com>
2024-06-04 13:24:30 +0100

    test-2

--- src/components/dashboard/Allproducts.jsx
+++ src/components/dashboard/Allproducts.jsx
@@ -1,8 +1,6 @@
 
-
 import { useEffect, useState } from "react";
-import Allproduct from "./Allproduct";
-
+import Allproduct from "./Allproduct"
 
 const Allproducts = () => {
   const [products, setProducts] = useState([]);
